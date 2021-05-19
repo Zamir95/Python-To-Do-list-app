24fd2e5959e4feb6c5fef15cd134d9102f473871
Zamir Lalji <47802606+Zamir95@users.noreply.github.com>
2021-05-19 12:01:12 -0400

    App Updated
    Uploaded a ToDo list python GUI app

---
+++ designer/app.py
@@ -0,0 +1,53 @@
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from main_window import Ui_MainWindow
+from dialog import Ui_Dialog
+from stylesheets import main_style_sheet
+from stylesheets import dialog_style_sheet
+
+class Dialog(QDialog):
+    def __init__(self, parent=None):
+        super(Dialog, self).__init__(parent)
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+        self.setStyleSheet(dialog_style_sheet)
+
+        
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        #vip
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.add_stuff) #push button to make new windows appear
+        self.done = [] #keep track of tasks one
+        self.not_done = [] #keep track of tasks that need to be completed
+    
+        self.pushButton_2.clicked.connect(self.do_task)
+        self.pushButton_3.clicked.connect(self.undo_task)
+        self.setStyleSheet(main_style_sheet)
+    def add_task(self, task): # will not print in the terminal but on the screen
+        if bool(task) != False:
+            self.remaining_list.addItem(task) 
+
+    def do_task(self):
+        task = self.remaining_list.takeItem(self.remaining_list.currentRow())#ramining list and takes item from row a returns back to task
+        if bool(task) != False:
+            self.finished_list.addItem(task.text())
+
+    def undo_task(self):
+        task = self.finished_list.takeItem(self.finished_list.currentRow())#ramining list and takes item from row a returns back to task
+        self.remaining_list.addItem(task.text())
+
+    def add_stuff(self):
+        dlg = Dialog()
+        dlg.ui.buttonBox.accepted.connect(
+            lambda: self.add_task(dlg.ui.new_task_input.text())
+        )
+        dlg.exec()
+
+
+app = QApplication([])
+window = MainWindow()
+window.show()
+app.exec()
